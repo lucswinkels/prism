c474c9a15a14e789f8de88100b8a666e0450bea6
Luc Swinkels <luc.swinkels@live.nl>
2020-10-28 14:23:42 +0100

    added animations, added link hrefs, added smooth scroll js for links, added link styling in footer, added navigation bar

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// Initialise AOS Library
+AOS.init();
+
+// Smooth Link Scrolling
+$(".smooth-scroll").click(function () {
+    $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        },
+        800
+    );
+    return false;
+});
